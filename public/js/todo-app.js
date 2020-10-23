fbdb901a4e266072555fd31513184fc5b80b4151
jared james <jaredjames.dev@gmail.com>
2020-10-23 16:25:38 +1300

    Created express server

---
+++ public/js/todo-app.js
@@ -0,0 +1,40 @@
+'use strict'
+
+let todos = fetchTodos()
+
+const filters = {
+  searchText: '',
+  hideCompleted: false
+}
+
+renderTodos(todos, filters)
+
+document.getElementById('new-todo').addEventListener('input', (e) => {
+  filters.searchText = e.target.value
+  renderTodos(todos, filters)
+})
+
+document.getElementById('todo-form').addEventListener('submit', (e) => {
+  e.preventDefault()
+  const result = e.target.elements.todoForm.value.trim()
+  if (result.length > 0) {
+  todos.push({
+    id: uuidv4(),
+    text: result,
+    completed: false
+  })
+} else {
+  return null
+}
+
+  saveTodos(todos)
+  renderTodos(todos, filters)
+  e.target.elements.todoForm.value = ''
+})
+
+
+document.getElementById('checked').addEventListener('change', (e) => {
+  filters.hideCompleted = e.target.checked
+  renderTodos(todos, filters)
+})
+
